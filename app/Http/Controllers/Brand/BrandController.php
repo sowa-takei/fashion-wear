5ca32afb6bfd46c9735109a864c136d4ede6b408
sowa-takei <souwasora@gmail.com>
2022-05-26 00:22:22 +0900

    ブランド作成

--- app/Http/Controllers/Brand/BrandController.php
+++ app/Http/Controllers/Brand/BrandController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers\Brand;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\brand;
 
 class BrandController extends Controller
 {
-    //
+    public function index()
+    {
+        $brands = Brand::get();
+        return view('brand.index',compact('brands'));
+    }
+
+    public function create (Request $request)
+    {
+        return view('brand.create'); 
+    }
 }
