177a49a0e326c4d2fc575a9e2b16243de8c8cb3c
sowa-takei <souwasora@gmail.com>
2022-05-30 17:51:07 +0900

    swiperチャレンジ中

--- resources/js/swiper.js
+++ resources/js/swiper.js
@@ -1,36 +1,33 @@
-// import Swiper JS
-
-// import Swiper styles
-import 'swiper/css';
-
-// core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  // direction: 'vertical',
-  loop: true,
-
+ // import Swiper JS
+ import Swiper, { Navigation, Pagination } from 'swiper';
+ // import Swiper and modules styles
+ import 'swiper/css';
+ import 'swiper/css/navigation';
+ import 'swiper/css/pagination';
+ 
+  // configure Swiper to use modules
+ Swiper.use([Navigation, Pagination]);
+ 
+  // init Swiper:
+ const swiper = new Swiper('.swiper', {
+ // Optional parameters
+ // direction: 'vertical',
+ loop: true,
+ 
   // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
-});
+ pagination: {
+ el: '.swiper-pagination',
+ },
+ 
+ // Navigation arrows
+ navigation: {
+ nextEl: '.swiper-button-next',
+ prevEl: '.swiper-button-prev',
+ },
+ 
+ // And if we need scrollbar
+ scrollbar: {
+ el: '.swiper-scrollbar',
+ },
+ });
+ 
