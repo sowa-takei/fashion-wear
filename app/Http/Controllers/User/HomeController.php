273499fc3d9f845de63a18dfc7c3979fd3b2a6e8
sowa-takei <souwasora@gmail.com>
2022-05-30 17:42:59 +0900

    マイページ編集画面作成

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -4,20 +4,43 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Item;
+use App\Models\User;
 
 class HomeController extends Controller
 {
     public function top()
     {
-        $items = Item::get();
+        $items = DB::table('items')->orderBy('id', 'desc')->take(4)->get();
         
         return view('home.top',compact('items'));
     }
-    public function index()
+    public function home()
     {
         $items = Item::get();
         
         return view('home',compact('items'));
     }
+
+    public function index()
+    {
+        $items = Item::get();
+        return view('home.index');
+    }
+
+    public function edit($id)
+    { 
+        $user = User::find($id);
+        return view('home.edit',compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->only(['name','name_kana', 'last_name', 'last_name_kana', 'postal_code', 'address', 'gender', 'telephone_number', 'emmail']));
+        return redirect()->route('home');
+    }
+
+    
 }
