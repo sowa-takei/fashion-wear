d8f0b5c772d257d94f5c06f25424422a9190885c
sowa-takei <souwasora@gmail.com>
2022-05-30 17:49:31 +0900

    ヘッダーマイぺージ表示

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,12 +36,18 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-                        <a href="{{ route('admin.login') }}">ログイン</a>
-                    </ul>
+                 
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('admin.login') }}">ログイン</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">マイページ</a>
+                        </li>
+
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
