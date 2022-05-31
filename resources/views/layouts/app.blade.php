950513b989c17ca67d16c7071b7acb73ae0de783
sowa-takei <souwasora@gmail.com>
2022-05-31 12:12:02 +0900

    style 追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/swiper.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/photo.css') }}" rel="stylesheet">
     
 </head>
 <body>
