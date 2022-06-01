786f02bc92214548f0a49d8781dca1ee42b4cbd8
sowa-takei <souwasora@gmail.com>
2022-06-01 22:23:41 +0900

    ブランド詳細画面

--- resources/views/home/show.blade.php
+++ resources/views/home/show.blade.php
@@ -17,7 +17,27 @@
                       <div class="row mt-5">
                           {{ $items->introduction }}
                       </div>
-                    </div>  
+                    </div>
+                    @auth
+                        <!-- Review.phpに作ったisLikedByメソッドをここで使用 -->
+                        @if (!$review->isLikedBy(Auth::user()))
+                            <span class="likes">
+                                <i class="fas fa-music like-toggle" data-review-id="{{ $item->id }}"></i>
+                            <span class="like-counter">{{$item->likes_count}}</span>
+                            </span><!-- /.likes -->
+                        @else
+                            <span class="likes">
+                                <i class="fas fa-music heart like-toggle liked" data-review-id="{{ $item->id }}"></i>
+                            <span class="like-counter">{{$item->likes_count}}</span>
+                            </span><!-- /.likes -->
+                        @endif
+                    @endauth
+                    @guest
+                        <span class="likes">
+                            <i class="fas fa-music heart"></i>
+                            <span class="like-counter">{{$item->likes_count}}</span>
+                        </span><!-- /.likes -->
+                    @endguest   
                 </div>
             </div>
           </div>
