2d6a99f54df94c450d1a3f1377bcfb2565a7ba40
sowa-takei <souwasora@gmail.com>
2022-06-02 11:06:34 +0900

    ブランド詳細画面から商品詳細遷移追加

--- resources/views/home/brand_show.blade.php
+++ resources/views/home/brand_show.blade.php
@@ -20,9 +20,27 @@
                 </div> 
             </div>
         </div>
-        @foreach ($items as $item)
-            <img src="{{ Storage::url($item->image_id) }}"  >
-        @endforeach
+        <div class="row">
+            <div class="col-12">
+                <div class="row mx-auto mt-5">
+                    @foreach ($items as $item)
+                        <div class="col-4">
+                            <div class="mx-auto">
+                                <div style="text-align:center;"><img src="{{ Storage::url($item->image_id) }}"  height="400px" ></div>
+                            </div>
+                            <a href="{{ route('user.show',['id'=>$item->id]) }}" style="cursor:default;color:inherit;text-decoration:none;">
+                                <div class="row mt-1" >
+                                    <div style="text-align:center;">{{ $item->name}}</div>
+                                </div>
+                                <div class="row mt-1">
+                                    <div style="text-align:center;"> ￥{{ $item->price}}</div>
+                                </div> 
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+        </div> 
     </div>
 </div>
 @endsection
