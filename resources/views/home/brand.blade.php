14e719c7cb3fab4bb2050197bbb6485b0e0326b2
sowa-takei <souwasora@gmail.com>
2022-06-07 12:42:24 +0900

    brand一覧写真追加

--- resources/views/home/brand.blade.php
+++ resources/views/home/brand.blade.php
@@ -4,196 +4,27 @@
 <div class="conteiner">
     <div class="card">
         <div class="card-header">{{ __('ブランド一覧画面') }}</div>
-        @include('layouts.sidebar')
+        
             <div class="row">
-                <div class="col-2"></div>
-                <div class="col-4">
-                    <div id="ini_list">
-                        <div class="ini">
-                            <ul>
-                                <li><span><a href="#ini_A">A</a></span></li>
-                                <li><span><a href="#ini_B">B</a></span></li>
-                                <li><span><a href="#ini_C">C</a></span></li>
-                                <li><span><a href="#ini_D">D</a></span></li>
-                                <li><span><a href="#ini_E">E</a></span></li>
-                                <li><span><a href="#ini_F">F</a></span></li>
-                                <li><span><a href="#ini_G">G</a></span></li>
-                                <li><span><a href="#ini_H">H</a></span></li>
-                                <li><span><a href="#ini_I">I</a></span></li>
-                                <li><span><a href="#ini_J">J</a></span></li>
-                                <li><span><a href="#ini_K">K</a></span></li>
-                                <li><span><a href="#ini_L">L</a></span></li>
-                                <li><span><a href="#ini_M">M</a></span></li>
-                                <li><span><a href="#ini_N">N</a></span></li>
-                                <li><span><a href="#ini_O">O</a></span></li>
-                                <li><span><a href="#ini_P">P</a></span></li>
-                                <li><span><a href="#ini_Q">Q</a></span></li>
-                                <li><span><a href="#ini_R">R</a></span></li>
-                                <li><span><a href="#ini_S">S</a></span></li>
-                                <li><span><a href="#ini_T">T</a></span></li>
-                                <li><span><a href="#ini_U">U</a></span></li>
-                                <li><span><a href="#ini_V">V</a></span></li>
-                                <li><span><a href="#ini_M">W</a></span></li>
-                                <li><span><a href="#ini_X">X</a></span></li>
-                                <li><span><a href="#ini_Y">Y</a></span></li>
-                                <li><span><a href="#ini_Z">Z</a></span></li>        
-                            </ul>
-                        </div>
-                    </div>
-                   
-                    <div id="ext" class="brandbox">
-                        <dl id="ini_A">
-                            <dt class="brand_ini">A</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00741/">ACANTHUS</a></dd>
-                            </div>
-                        </dl>
+            @include('layouts.sidebar')
+                <div class="col-10">
+                    <div class="row mx-auto mt-3">
+                        @foreach ($brands as $brand)
+                                <div class="col-5">
+                                    <a href="{{ route('user.brand_show',['id'=>$brand->id]) }}" style="cursor:default;color:inherit;text-decoration:none;">
+                                        <div class="mx-auto">
+                                            <div style="text-align:center;"><img src="{{ Storage::url($brand->image_id) }}"  height="400px" ></div>
+                                        </div>
+                                        <div class="row mt-1" >
+                                            <div style="text-align:center;">{{ $brand->name}}</div>
+                                        </div>
                         
-                        <dl id="ini_B">
-                            <dt class="brand_ini">B</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00703/">BASE MARK</a></dd>
-                            </div>
-                        </dl>
-                        
-                        <dl id="ini_C">
-                            <dt class="brand_ini">C</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00663/">CHICSTOCKS</a></dd>
-                            </div>
-                        </dl>
-                        
-                        <dl id="ini_D">
-                            <dt class="brand_ini">D</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00716/">DAIRIKU</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_E">
-                            <dt class="brand_ini">E</dt>
-                            
-                        </dl>
-                        <dl id="ini_F">
-                            <dt class="brand_ini">F</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00644/">FUMITO GANRYU</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_G">
-                            <dt class="brand_ini">G</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00620/">Ground Y</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_H">
-                        <dt class="brand_ini">H</dt>
-                            
-                        </dl>
-                        <dl id="ini_I">
-                            <dt class="brand_ini">I</dt>
-                            
-                        </dl>
-                        <dl id="ini_J">
-                            <dt class="brand_ini">J</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00085/">JOHN LAWRENCE SULLIVAN</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_K">
-                            <dt class="brand_ini">K</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00711/">KIDILL</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_L">
-                            <dt class="brand_ini">L</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00109/">LAD MUSICIAN</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_M">
-                            <dt class="brand_ini">M</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00662/">Maison Margiela</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_N">
-                            <dt class="brand_ini">N</dt>
-                            
-                        </dl>
-                        <dl id="ini_O">
-                            <dt class="brand_ini">O</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00138/">Off-White</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_P">
-                            <dt class="brand_ini">P</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00141/">PADRONE</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_Q">
-                            <dt class="brand_ini">Q</dt>
-                            
-                        
-                        </dl>
-                        <dl id="ini_R">
-                            <dt class="brand_ini">R</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00161/">RAF SIMONS</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_S">
-                            <dt class="brand_ini">S</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00709/">SAINT Mxxxxxx</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_T">
-                            <dt class="brand_ini">T</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00190/">TAKAHIROMIYASHITATheSoloist.</a></dd>
-                            </div>
-                        </dl>
-                        <dl id="ini_U">
-                            <dt class="brand_ini">U</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00701/">UNDECORATED</a></dd>
-                            </div>     
-                        </dl>
-                        <dl id="ini_V">
-                            <dt class="brand_ini">V</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00653/">VITAL MATERIAL</a></dd>
-                            </div>
-                        
-                        </dl>
-                        <dl id="ini_M">
-                            <dt class="brand_ini">W</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00660/">WE11DONE</a></dd>
-                            </div>
-                        
-                        </dl>
-                        <dl id="ini_X">
-                            <dt class="brand_ini">X</dt>
-                            
-                        
-                        </dl>
-                        <dl id="ini_Y">
-                            <dt class="brand_ini">Y</dt>
-                            <div>
-                                <dd><a href="https://store-midwest.com/category/00218/">yoshiokubo</a></dd>
-                            </div>
-                        
-                        </dl>
-                        <dl id="ini_Z">
-                            <dt class="brand_ini">Z</dt>
-                           
-                        </dl>
-                    </div>                            
-                </div>
+                                    </a>
+                                </div>
+     
+                        @endforeach
+                    </div>   
+                </div>     
             </div>
         </div>
     </div>
