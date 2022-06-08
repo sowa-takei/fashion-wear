2644f99defb96b864c15d1b33fa573c7e51a3ad9
sowa-takei <souwasora@gmail.com>
2022-06-08 16:36:13 +0900

    レイアウト変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,50 +7,47 @@
             <div class="card">
                 <div class="card-header">{{ __('編集画面') }}
                     <div class="card-body">
-            <table class="table table-borderless">
-                <thead class="thead-light">
-                    <tr>
-                        <th>氏名</th>
-                        <td>{{ Auth::user()->name }}{{ Auth::user()->last_name }} </td>
-                    </tr>
-                    <tr>
-                        <th>フリガナ</th>
-                        <td>{{ Auth::user()->name_kana }}{{ Auth::user()->last_name_kana }}</td>
-                    </tr>
-                    <tr>
-                        <th>郵便番号</th>
-                        <td>{{ Auth::user()->postal_code }}</td>
-                    </tr>
-                    <tr>
-                        <th>住所</th>
-                        <td>{{ Auth::user()->address }}</td>
-                    </tr>
-                    <tr>
-                        <th>電話番号</th>
-                        <td>{{ Auth::user()->telephone_number }}</td>
-                    </tr>
-                    <tr>
-                        <th>メールアドレス</th>
-                        <td>{{ Auth::user()->email }}</td>
-                    </tr>
-                    <tr>
-                        <th>性別</th>
-                        <td> {{ Auth::user()->gender }}</td>
-                    </tr>
-                    <tr>
-                        <th>ID</th>
-                        <td> {{ Auth::user()->id }}</td>
-                    </tr>
-                </thead>
-
-            </table>
-            <a href="{{ route('user.edit',['id=>$user->id']) }}" class="btn btn-info">編集</a>
-        
-
+                        <table class="table table-borderless">
+                            <thead class="thead-light">
+                                <tr>
+                                    <th>氏名</th>
+                                    <td>{{ Auth::user()->name }}{{ Auth::user()->last_name }} </td>
+                                </tr>
+                                <tr>
+                                    <th>フリガナ</th>
+                                    <td>{{ Auth::user()->name_kana }}{{ Auth::user()->last_name_kana }}</td>
+                                </tr>
+                                <tr>
+                                    <th>郵便番号</th>
+                                    <td>{{ Auth::user()->postal_code }}</td>
+                                </tr>
+                                <tr>
+                                    <th>住所</th>
+                                    <td>{{ Auth::user()->address }}</td>
+                                </tr>
+                                <tr>
+                                    <th>電話番号</th>
+                                    <td>{{ Auth::user()->telephone_number }}</td>
+                                </tr>
+                                <tr>
+                                    <th>メールアドレス</th>
+                                    <td>{{ Auth::user()->email }}</td>
+                                </tr>
+                                <tr>
+                                    <th>性別</th>
+                                    <td> {{ Auth::user()->gender }}</td>
+                                </tr>
+                                <tr>
+                                    <th>ID</th>
+                                    <td> {{ Auth::user()->id }}</td>
+                                </tr>
+                            </thead>
+                        </table>
+                        <a href="{{ route('user.edit',['id=>$user->id']) }}" class="btn btn-info">編集</a>
+                    </div>
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
-</div>
     </div>
 </div>
 @endsection
