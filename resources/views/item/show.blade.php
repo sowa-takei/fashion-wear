bef3457d0ca1424d8767a02aadc762489a55feb9
sowa-takei <souwasora@gmail.com>
2022-06-09 01:19:53 +0900

    nl2br追加

--- resources/views/item/show.blade.php
+++ resources/views/item/show.blade.php
@@ -4,27 +4,35 @@
       <div class="card">
         <div class="card-header">{{ __('商品詳細画面') }}</div>
             <div class="row">
-                <div class="col-4">
-                    <img src="{{ Storage::url($items->image_id) }}" width="100%" >
+                <div class="col-3">
+                    <img src="{{ Storage::url($items->image_id) }}" height="400px" >
                 </div>
-                <div class="col-8">
+                <div class="col-7">
                     <table class="table table-borderless"> 
-
                         <tr>
                             <th>商品名</th>
                             <td>{{ $items->name}}</td>
-                        </tr>
-                        <tr>
-                            <th>商品詳細</th>
-                            <td>{!! nl2br(htmlspecialchars($items->introduction )) !!}</td>
                         </tr>
                         <tr>
                             <th>値段</th>
                             <td>{{ $items->price }}円</td>     
                         </tr>
                     </table>
+                    <table class="table table-borderless"> 
+                        <tr>
+                            <th>ブランド詳細</th>
+                        </tr>
+                        <tr>
+                            <td>{!! nl2br(htmlspecialchars($items->introduction )) !!}</td>
+                        </tr>
+                    </table>    
                 </div>
             </div>
+            
+              
+                       
+                
+            
           </div>
     </div>
 </div>
