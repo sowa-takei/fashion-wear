bca3a1a8b6e40ed762065a01b99e15bcc5b00a7d
sowa-takei <souwasora@gmail.com>
2022-06-09 19:54:51 +0900

    バリデーション追加

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -25,11 +25,12 @@ class UserRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'name_kana' => ['required', 'string', 'max:255'],
+            'name_kana' => ['regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
-            'last_name_kana' => ['required', 'string', 'max:255'],
+            'last_name_kana' => ['regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u', 'string', 'max:255'],
             'address' => ['required', 'string', 'max:255'],
-            'postal_code' => ['required', 'string', 'max:7', 'min:7'],
+            'postal_code' => ['required', 'string', 'max:7', 'min:7','regex:/^[0-9]+$/'],
+            'telephone_number' => ['required', 'string', 'max:13','min:12','regex:/^[0-9-]+$/'],
         ];
     }
 }
