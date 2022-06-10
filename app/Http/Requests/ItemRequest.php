3bae5bf6b7510daf1914eb0e2fc873786f1d24f4
sowa-takei <souwasora@gmail.com>
2022-06-10 20:10:20 +0900

    request バリデーション

---
+++ app/Http/Requests/ItemRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ItemRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'introduction' => ['required', 'string', 'max:2000'],
+            'price' => ['required', 'string', 'max:7', 'min:4','regex:/^[0-9]+$/'],
+        ];
+    }
+}
